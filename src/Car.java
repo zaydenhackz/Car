028a88a5aaa7189f76c7dd896334b599e7423744
zaydenhackz <marvin20867@gmail.com>
2022-12-17 00:52:38 -0500

    added the car class and initializer

---
+++ src/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+
+    String manufacturer;
+    String model;
+    String color;
+    int year;
+
+
+
+         Car(String manufacturer, String model, String color, int year){
+             this.manufacturer = manufacturer;
+             this.model = model;
+             this.color = color;
+             this.year = year;
+
+         }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car Jamires_car = new Car("Dodge", "Durango", "Black", 2018);
+
     }
 }
